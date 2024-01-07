5b8623185320681befbf2aa78e7248c3de73b3ab
David Egbuwalo <lereegbuwalo@gmail.com>
2024-01-07 15:51:36 +0100

    update

---
+++ src/test/java/OOPConcept/Dog.java
@@ -0,0 +1,11 @@
+package OOPConcept;
+
+public class Dog {
+    private String name = "Simba";
+
+    private String breed = "German Shepherd";
+
+    private String Bark(){
+        return (name + "barks");
+    }
+}
